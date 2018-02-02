a0a0c498f83828c76da190498a181846d1179c4e
Jeremy Mack Wright <wrightjmf@gmail.com>
2018-02-02 17:31:22 -0500

    Fixed background styling and added origin indicator back in.

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -41,7 +41,42 @@ var VIEWER = function() {
       scene.add( light );
 
       // axes
-      scene.add( new THREE.AxisHelper( 20 ) );
+      // scene.add( new THREE.AxisHelper( 20 ) );
+
+      // Set up the origin indicator
+      originContainer = document.getElementById('indicator');
+      originRenderer = new THREE.WebGLRenderer({
+              antialias   : true, // to get smoother output
+              preserveDrawingBuffer : true  // to allow screenshot
+          });
+      originRenderer.setClearColor(0xf0f0f0, 1);
+      originRenderer.setSize(100.0, 100.0);
+      originRenderer.setClearColor(settings.clearColor, 1);
+      originContainer.appendChild(originRenderer.domElement);
+      originScene = new THREE.Scene();
+      originCamera = new THREE.PerspectiveCamera(35, 100.0 / 100.0, 1, 1000);
+      originCamera.up = camera.up;
+
+      // Set up the axes for the origin indicator
+      var dirX = new THREE.Vector3(100, 0, 0);
+      var dirY = new THREE.Vector3(0, 100, 0);
+      var dirZ = new THREE.Vector3(0, 0, 100);
+      var origin = new THREE.Vector3(0, 0, 0);
+      var length = 75;
+      var xAxisColor = 0xff0000;
+      var yAxisColor = 0x00ff00;
+      var zAxisColor = 0x0000ff;
+
+      var arrowHelperX = new THREE.ArrowHelper(dirX, origin, length, xAxisColor, 20, 15);
+      var arrowHelperY = new THREE.ArrowHelper(dirY, origin, length, yAxisColor, 20, 15);
+      var arrowHelperZ = new THREE.ArrowHelper(dirZ, origin, length, zAxisColor, 20, 15);
+      originScene.add(arrowHelperX);
+      originScene.add(arrowHelperY);
+      originScene.add(arrowHelperZ);
+
+      // Base grid helps us orient ourselves
+      var baseGrid = new THREE.GridHelper( 30, 1 );
+      scene.add(baseGrid)
 
       // geometry
       var geometry = new THREE.SphereGeometry( 5, 12, 8 );
@@ -62,7 +97,14 @@ var VIEWER = function() {
   }
 
   function render() {
+      // Main scene
       renderer.render( scene, camera );
+
+      // Origin scene
+      originCamera.position.copy(camera.position);
+      originCamera.lookAt(originScene.position);
+      originCamera.position.setLength(300);
+      originRenderer.render(originScene, originCamera);
   }
 
   //exported stuff
